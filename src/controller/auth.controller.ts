31039bba1d7a840533ae04747998056aee660b04
Rizwan <mhdrizwanpkd@gmail.com>
2025-01-22 15:06:36 +0530

    fix:cookie

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -42,14 +42,12 @@ export async function login(
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: true,  
-      sameSite: "strict",
       maxAge: 15 * 60 * 1000,
     });
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
