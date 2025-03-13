1afe4f2a3ea961cc4840afde2bba7a4e9d7bf532
Riswan <mhdrizwanpkd@gmail.com>
2025-03-13 16:03:39 +0530

    changed the bug

--- src/adapter/database/user.model.ts
+++ src/adapter/database/user.model.ts
@@ -5,8 +5,8 @@ const userSchema = new Schema<User>(
   {
     firstName: { type: String, required: true, minlength: 2 },
     lastName: { type: String, required: true, minlength: 2 },
-    phone: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true},
+    email: { type: String, required: true },
     dob: { type: Date, required: true },
     password: { type: String, required: true, minlength: 8 },
     is_verified: { type: Boolean, default: false },
